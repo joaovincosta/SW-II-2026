27e90e1d9bf3a5b9da5f35a2d97692559a839e4a
João Vinícius Costa <161096439+joaovincosta@users.noreply.github.com>
2026-04-02 08:48:39 -0300

    Add files via upload

---
+++ aula05/Exercicio01.php
@@ -0,0 +1,16 @@
+<?php
+    // array com 3 produtos
+    $json_str = '
+        {"produtos":  
+            [
+                {"nome":"Milho", "preco":6, "qntd":100},
+                {"nome":"Dolly Guaraná", "preco":6, "qntd":70},
+                {"nome":"Caixa Paçoquita 10un", "preco":10, "qntd":30}
+            ]
+        }';
+
+    // conversão do array para JSON com json_encode()
+    
+
+    // salvamento do JSON em um arquivo com file_put_contents()
+?>
